13d5c64227c12151ddc803539641cd651c8c94a7
Simon Bader <simonbader@yahoo.com>
2017-05-23 23:54:40 +0200

    first commit

---
+++ BrowserHistory/Controllers/ChildController.cs
@@ -0,0 +1,32 @@
+﻿using BrowserHistory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrowserHistory.Controllers
+{
+    public class ChildController : Controller
+    {
+        private static string _firstName;
+
+        // GET: Child
+        public ActionResult Index()
+        {
+            return View(new Person() { FirstName = "Index" });
+        }
+
+        //[HttpPost]
+        public ActionResult Create(string firstName)
+        {
+            _firstName = firstName;
+            return RedirectToAction("Result");
+        }
+
+        public ActionResult Result()
+        {
+            return View(new Person() { FirstName=_firstName});
+        }
+    }
+}
